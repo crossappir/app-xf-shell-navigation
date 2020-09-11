8510f31b99d9f387c02e1a79f41cdfbe78127798
cross app <crossapp.info@gmail.com>
2020-09-11 19:35:27 +0430

    First Commit

---
+++ App35/App35/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using App35.Views;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App35
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(Page2), typeof(Page2));
+            Routing.RegisterRoute(nameof(Page3), typeof(Page3));
+        }
+    }
+}
---
+++ App35/App35/Views/Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App35.Views
+{
+    [QueryProperty(nameof(Data), nameof(Data))]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+        public int Count { get; set; }
+        
+        void ButtonClick_Clicked(object sender, EventArgs e)
+        {
+            Count++;
+            OnPropertyChanged(nameof(Count));
+        }
+
+        async void ButtonNavigate_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"{nameof(Page2)}?Count={Count}");
+        }
+
+        string data = "";
+        public string Data
+        {
+            get => data;
+            set
+            {
+                data = Uri.UnescapeDataString(value ?? string.Empty);
+                OnPropertyChanged();
+            }
+        }
+    }
+}
